61ea6361cb5e2f5aaf290c72ee8811f2f4fd0854
Yuriel849 <yuriel849sp@gmail.com>
2019-10-04 15:54:54 +0200

    first lab assignments

---
+++ [Lab01] Getting Started/lab1a.c
@@ -0,0 +1,27 @@
+/* Author	   : Myungjun Kim
+   Contents    : Implementation of a simple calculator in C for arithmetic operations
+   Instructions: 1. Define two integer variables with values 7 and 3, respectively.
+				 2. Generate the console output below.
+					a) The printf() function must insert the operand values 7 and 3 from the integer variables.
+					b) The results must be calculated using algorithmic operations.
+ */
+
+#include <stdio.h>
+
+int lab1a_main()
+{
+	int seven = 7, three = 3;
+
+	printf("Simple calculator : \n");
+	printf("------------------\n");
+	printf("1st operand: %d\n", seven);
+	printf("2nd operand: %d\n\n", three);
+	printf("%d + %d = %d\n", seven, three, seven + three);
+	printf("%d - %d = %d\n", seven, three, seven - three);
+	printf("%d * %d = %d\n", seven, three, seven * three);
+	printf("%d / %d = %d\n", seven, three, seven / three);
+	printf("%d %% %d = %d", seven, three, seven % three);
+
+	getchar();
+	return 0;
+}
