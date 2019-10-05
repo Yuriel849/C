107fcd975ebf25d2622ba8eaa0b0097d0bfe3cbb
Yuriel849 <yuriel849sp@gmail.com>
2019-10-05 12:36:32 +0200

    modify to use a while loop

--- [Lab01] Getting Started/lab1c.c
+++ [Lab01] Getting Started/lab1c.c
@@ -15,6 +15,15 @@ int main()
 	scanf("%d", &year); // The user enters a 4-digit number.
 	getchar();
 
+
+	//// While a year has only 4 digits, it is possible that a number with more digits may be entered, so a loop is used to handle numbers with any number of digits
+	//while (year != 0) {
+	//	// Get the remainder of the year entered by the user when divided by 10
+	//	sum += year % 10;
+	//	// Divide the year by 10 to remove the rightmost digit
+	//	year /= 10;
+	//}
+
 	// Get the remainder of the year entered by the user when divided by 10
 	sum += year % 10;
 	// Divide the year by 10 to remove the rightmost digit
