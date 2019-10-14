bfeb835d9bff15fa25278f4e08aa2d14688196ac
Yuriel849 <yuriel849sp@gmail.com>
2019-10-15 00:26:51 +0200

    add instructions in comments

---
+++ [Lab02] Data Types and Control Flow/lab2a.c
@@ -0,0 +1,21 @@
+/* Author	   : Myungjun Kim
+   Contents    : Print a chessboard in a two-dimensional order to the console.
+   Instructions: 1. Use for loops to print all fields of a chessboard in a two-dimensional order to the console.
+				 2. Rows are labeled 8 (top) to 1 (bottom).
+				 3. Columns are labeled a (left) to h (right).
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	// Use two for loops, one inside the other.aa
+	// The other loop prints each row.
+	// The inner loops prints the contents of each row.
+	// If it is an odd-numbered row, print the pluses and dashes.
+	// If it is an even-numbered row, print the bars and identifiers for each place.
+	// Two separate variables, one to represent the row number and the other to represent the column letter.
+	// Reduce the row number by one every time the outer loop runs.
+	// Increase the column letter by one every time the inner loop runs in an even-numbered row, then reset.
+
+}
