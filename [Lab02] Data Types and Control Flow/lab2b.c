a5de39ae88b50fe2709ffbab46d066830c4ab63d
Yuriel849 <yuriel849sp@gmail.com>
2019-10-18 10:41:46 +0200

    update instructions

--- [Lab02] Data Types and Control Flow/lab2b.c
+++ [Lab02] Data Types and Control Flow/lab2b.c
@@ -3,7 +3,11 @@
    Instructions: 1. Print a table with the following information.
 					a. Sequential numbering of the chessboard fields (1 to 64)
 					b. Number of grains on the specific field (i. e., 1, 2, 4, 8, . . . )
-					c. Sum of grains on all fields up to the specific field (e. g., 1 + 2 + 4 = 7 for the third field)   
+					c. Sum of grains on all fields up to the specific field (e. g., 1 + 2 + 4 = 7 for the third field)
+				 2. Vary data types used for number of grains to be unsigned short, unsigned, unsigned long and unsigned long long.
+					Explain observations and determine number of bits used on this machine to represent each of these data types.
+				 3. Determine overall weight of all wheat grains (assuming weight of 55 mg / grain).
+				 4. How does overall weight of all grains relate to weight of worldwide wheat production in 2015, about 735.8 million tons?
  */
 
 #include <stdio.h>
