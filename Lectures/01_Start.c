b2c04bd94fde6dc564bdd35ee1617945d4083f11
Yuriel849 <yuriel849sp@gmail.com>
2019-10-22 09:53:06 +0200

    main() <--> main(void)

--- Lectures/01_Start.c
+++ Lectures/01_Start.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int S_main() // There can only be ONE(1) main() in a project
+/*
+	main() != main(void)
+		() means any or no argument is accepted
+		(void) means there is no parameter at all and thus no argument is accepted
+ */
+
+int S_main(void) // There can only be ONE(1) main() in a project
 {
 	// all commands within a function are called statements and must end with a semicolon (";")
 	printf("So..... the first \'program\' for this course in C :)");
