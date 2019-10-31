55ca65ef97f9f7bd9acbe84be81886226ce3bde4
Yuriel849 <yuriel849sp@gmail.com>
2019-10-31 06:36:52 +0100

    new solution for lab3

---
+++ [Lab03] Functions/lab3/lab3.c
@@ -0,0 +1,29 @@
+/* Author	   : Myungjun Kim
+   Contents    : Calculate the hemisphere
+   Instructions: 1. Hemispheres
+					a) Implement a function isNorthernHemisphere() that returns a logical true only if the geographic location passed to the function is on the northern hemisphere
+					b) Implement a function isSouthernHemisphere() accordingly.
+					c) Write a program that uses some test coordinates to verify the correct return values.
+ */
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <math.h>
+
+char isNorthernHemisphere(double, double);
+char isSouthernHemisphere(double, double);
+
+int main(void)
+{
+
+}
+
+char isNorthernHemisphere(double latitude, double longitude)
+{
+
+}
+
+char isSouthernHemisphere(double latitude, double longitude)
+{
+
+}
