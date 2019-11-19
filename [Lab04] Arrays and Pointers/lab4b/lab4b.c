3c28f2ab8298146f0a83d377776d8a5c4672a087
Yuriel849 <yuriel849sp@gmail.com>
2019-11-19 12:24:34 +0100

    2nd lab assignment

---
+++ [Lab04] Arrays and Pointers/lab4b/lab4b.c
@@ -0,0 +1,18 @@
+/* Author	   : Myungjun Kim
+   Contents    : Determine which of the two locations are furthest from each other
+   Instructions:
+	  a) Store the locations¡¯ names in an array of strings (e. g., char *names[] = { ... };).
+	  b) Store the geographic coordinates in a 7 ¡¿ 2 matrix. Each row contains the latitude and longitude of a location.
+	  c) Write a function that receives, amongst others, a Mx2 matrix of coordinates as parameter. The function shall determine which two locations in the matrix have the largest distance. It returns the maximum distance as well as the row indices of the corresponding locations
+	  d) Print the locations¡¯ names and distance to the console.
+ */
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <math.h>
+#include <corecrt_math_defines.h>
+
+int main(void)
+{
+
+}
