79db45e58d00fab1423a4ebf621eaf00aeee1224
Yuriel849 <yuriel849sp@gmail.com>
2019-12-07 15:42:58 +0100

    migrate exercise files to VS2017

---
+++ SO1-Exercises/SO1-WBook-Exercise22/WBook-Exercise22.c
@@ -0,0 +1,14 @@
+/* Author	   : Myungjun Kim
+   Contents    : Exercise 22.
+   Instructions: Write a function to calculate the mean value x of a vector x = (x1, x2, . . . , xN )T
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+
+	getchar();
+	return 0;
+}
