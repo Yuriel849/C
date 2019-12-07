904dd3b413b13e59bac1037aab03bab5ded6434a
Yuriel849 <yuriel849sp@gmail.com>
2019-12-07 19:25:54 +0100

    working code completed (requires improvement, esp. coordinates input)

--- SO1-Labs/lab5a/lab5a.c
+++ SO1-Labs/lab5a/lab5a.c
@@ -10,6 +10,7 @@
 		 Dynamically allocate 1-D arrays to hold the latitudes and the longitudes of the waypoints.
 		 Make sure to free dynamically allocated memory before the application exits
  */
+
 #define _CRT_SECURE_NO_DEPRECATE	// to use scanf() without warnings
 #define _USE_MATH_DEFINES			// to use math constants (i.e. PI)
 
@@ -24,75 +25,109 @@ double distanceKm(double latitudeX, double longitudeX, double latitudeY, double 
 
 int main(void)
 {
-	char inputOne[6], inputTwo[6];// Variables to receive user input as strings, to then be parsed
-	int waypntNumber; // Variable for the number of waypoints
-	double *latArr, *longArr; // 1-D arrays to hold latitudes and longitudes
-	double totalDistance = 0.0; // Variable for the total distance between all waypoints
-	int strLength;
+	char inputOne[6], inputTwo[6];	// Variables to receive user input as strings
+	int waypntNumber;				// Variable for the number of waypoints
+	double *latArr, *longArr;		// 1-D arrays to hold latitudes and longitudes
+	double totalDistance = 0.0;		// Variable for the total distance between all waypoints
+	int strLength;					// Variable to store the length of strings in
+	int flagContinue = 0;			// Flag variable
 
 	// User input: Request the number of waypoints
 	printf("Enter number of waypoints : ");
 	// Check that the user enters a number and nothing else (i.e. text)
-	while (1) // Validation of waypoint number input
+	while (1)
 	{
 		scanf("%2s", inputOne); // Reads a string which is maximum two characters long
 		while (getchar() != '\n') // Clears the input buffer
 			continue;
-		strLength = strlen(inputOne); // Gets length of the input string, to know if it is one or two characters long
+		strLength = strlen(inputOne); // Gets length to know if input is one or two characters long
 		if (strLength == 1 && isdigit(inputOne[0])) // If one character long and that is a digit, break
 		{
-			waypntNumber = inputOne[0] - '0';
+			waypntNumber = atoi(inputOne); // Convert string into integer
 			break;
 		}
 		else if (isdigit(inputOne[0]) && isdigit(inputOne[1])) // If first and second characters are both digits, break
 		{
-			waypntNumber = (inputOne[0] - '0') * 10 + (inputOne[1] - '0');
+			waypntNumber = atoi(inputOne); // Convert string into integer
 			break;
 		}
 
 		printf("Try again (expected number >= 0) : ");
 	}
 
-
 	// Create two 1-D arrays with as many elements as the number of waypoints
 	latArr = (double *)calloc(waypntNumber, sizeof(double));
 	longArr = (double *)calloc(waypntNumber, sizeof(double));
 	if (latArr == NULL || longArr == NULL)
 		exit(EXIT_FAILURE);
+
 	// User input: Request as many pairs of geographic coordinates as the number of waypoints
 	printf("Enter waypoints as \"<latitudes> <longitude>\" : \n");
 	for (int i = 0; i < waypntNumber; i++)
 	{
-		printf("Waypoint %d : ", i);
-		// Check that both coordinates are proper floating-point numbers between +90 & -90 degrees for latitude and +180 & -180 degrees for longitude, and nothing else (i.e. text)
+		printf("Waypoint %d : ", i + 1);
+		// Check that coordinates are valid double numbers
 		while (1)
 		{
-			scanf("%5s %5s", inputOne, inputTwo); // Reads two string which are each maximum five characters long (excluding \0)
+			scanf("%5s %5s", inputOne, inputTwo); // Reads two strings which are each maximum five characters long (excluding \0)
 			while (getchar() != '\n') // Clears the input buffer
 				continue;
+			//printf("CHECK PRINT STRINGS : %s %s\n", inputOne, inputTwo);
+			//printf("CHECK PRINT NUMBERS : %f %f\n", atof(inputOne), atof(inputTwo);
 
-			strLength = strlen(inputOne); // Gets length of the input string, to know if it is one or two characters long
-			if (strLength == 1 && isdigit(inputOne[0])) // If one character long and that is a digit, break
+			strLength = strlen(inputOne);
+			// Loop through both input strings, check that there are only numbers
+			for (int j = 0; j < strLength; j++)
 			{
-				waypntNumber = inputOne[0] - '0';
-				break;
+				// If there is a character, break
+				if (!(isdigit(inputOne[j]) || inputOne[j] == '.' || inputOne[j] == '-'))
+				{
+					flagContinue = 1;
+					break;
+				}
 			}
-			else if (isdigit(inputOne[0]) && isdigit(inputOne[1])) // If first and second characters are both digits, break
+			if (flagContinue == 1)
 			{
-				waypntNumber = (inputOne[0] - '0') * 10 + (inputOne[1] - '0');
-				break;
+				flagContinue = 0;
+				printf("Invalid input (expected \"<latitude> <longitude>\": %s\nTry again : ", inputOne);
+				continue;
+			}
+			strLength = strlen(inputTwo);
+			// Loop through both input strings, check that there are only numbers
+			for (int j = 0; j < strLength; j++)
+			{
+				// If there is a character, break
+				if (!(isdigit(inputTwo[j]) || inputTwo[j] == '.' || inputOne[j] == '-'))
+				{
+					flagContinue = 1;
+					break;
+				}
+			}
+			if (flagContinue == 1)
+			{
+				flagContinue = 0;
+				printf("Invalid input (expected \"<latitude> <longitude>\": %s\nTry again : ", inputTwo);
+				continue;
 			}
 
-			printf("Invalid input (expected \"<latitude> <longitude>\": %s\ntry again : ", inputOne);
+			// Check that latitude coordinate is between +90 & -90 degrees and longitude coordinate is between + 180 & -180 degrees
+			latArr[i] = atof(inputOne);
+			longArr[i] = atof(inputTwo);
+			if (!(latArr[i] >= -90. && latArr[i] <= 90.))
+				printf("Invalid input (expected \"<latitude> <longitude>\": %s\nTry again : ", inputOne);
+			else if (!(longArr[i] >= -180. && longArr[i] <= 180.))
+				printf("Invalid input (expected \"<latitude> <longitude>\": %s\nTry again : ", inputTwo);
+			else
+				break;
 		}
 	}
 
-	// Run a loop to call a function determine the distance between two sets of coordinates, and sum up all distances
+	// Run a loop to call a function to determine the distance between two sets of coordinates, and sum up all distances
 	for (int i = 1; i < waypntNumber; i++)
 		totalDistance += distanceKm(latArr[i - 1], longArr[i - 1], latArr[i], longArr[i]);
 
 	// Print the total distance between the coordinates
-	printf("By taking this route you will travel %f km.", totalDistance);
+	printf("By taking this route you will travel %.1f km.", totalDistance);
 
 	// Free memory allocated to arrays holding waypoint coordinates
 	free(latArr);
