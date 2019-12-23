f98f26814ef75581bc833bf4a01b26d1c7e9bd14
Yuriel849 <yuriel849sp@gmail.com>
2019-12-23 22:38:47 +0100

    exercise 22 complete

--- SO1-Exercises/Exercise 22/Exercise22.c
+++ SO1-Exercises/Exercise 22/Exercise22.c
@@ -4,11 +4,31 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int calculateMean(int *vector, int size);
 
 int main(void)
 {
+	int x[7] = { 1, 3, 5, 7, 9, 11, 13 }; // mean is sum divided by number => here, mean is 7
+	int result = 0;
 
+	result = calculateMean(x, 7);
+
+	printf("This is the mean vaue of the vector x : %d", result);
 
 	getchar();
 	return 0;
+}
+
+int calculateMean(int *vector, int size)
+{
+	int mean = 0;
+
+	for (int i = 0; i < size; i++)
+		mean += vector[i];
+
+	mean /= size;
+
+	return mean;
 }
