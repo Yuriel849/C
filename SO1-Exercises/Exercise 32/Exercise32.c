c508c2de12c0dbf8501f2659358d7b24a09e86d3
Yuriel849 <yuriel849sp@gmail.com>
2019-12-27 17:47:27 +0100

    exercise 32 complete

--- SO1-Exercises/Exercise 32/Exercise32.c
+++ SO1-Exercises/Exercise 32/Exercise32.c
@@ -6,16 +6,16 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int getStringLength(char *string);
 int toInteger(char *string, int length);
 
 int main(void)
 {
-	char *toConvert = "123456";
+	char *toConvert = "-123456";
 	int length = getStringLength(toConvert);
 	int convertedInteger = toInteger(toConvert, length);
-	printf("%d", length);
 	printf("String %s => Integer %d", toConvert, convertedInteger);
 
 	getchar();
@@ -25,15 +25,25 @@ int main(void)
 int toInteger(char *string, int length)
 {
 	int result = 0;
+	int counter = 0;
 
-	while (length-- != 1)
-	{
-		// If the string contains what is not a negative sign or a digit, break
-		if (string[length] != '-' || string[length] != '0' || string[length] != '1' ||
-			string[length] != '2' || string[length] != '3' || string[length] != '4' ||
-			string[length] != '5' || string[length] != '6' || string[length] != '7' ||
-			string[length] != '8' || string[length] != '9')
+	// First check that the string consists of only a negative sign and digits
+	for (int i = 0; i < length; i++)
+		if ((i != 0 && string[i] == '-') ||
+			string[i] != '0' || string[i] != '1' || string[i] != '2' || string[i] != '3' || string[i] != '4' ||
+			string[i] != '5' || string[i] != '6' || string[i] != '7' || string[i] != '8' || string[i] != '9')
 			break;
+
+	// Convert the string into a matching integer
+	while (length-- != 0)
+	{
+		// Check if the first character is a negative sign, and if yes, break
+		if (length == 0 && string[length] == '-')
+		{
+			result *= -1;
+			break;
+		}
+		result += (string[length] - '0') * (int) pow(10, counter++);
 	}
 
 	return result;
