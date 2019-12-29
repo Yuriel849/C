d82d9a538b3d6900e8b679a776399d39fbef8c1b
Yuriel849 <yuriel849sp@gmail.com>
2019-12-29 16:42:46 +0100

    lab6a basic setup

---
+++ SO1-Labs/lab6a/lab6a.c
@@ -0,0 +1,40 @@
+/*
+Author	    : Myungjun Kim
+Contents    : Implementation of the seating plan for the Bombardier CRJ-200 aircraft.
+Instructions:
+	The CRJ-200 has 50 seats and every row, except for the last one, has 4 seats. The last row consists of only 2 seats.
+	Every seat is either vacant or reserved.
+Requirements:
+	Information to be printed to the console:
+		Reservation status (vacant or reserved) for each seat
+		Overall number of vacant seats and number of reserved seats
+	Making reservations:
+		Initially all seats are vacant.
+		Users are repeatedly prompted to reserve a seat. If vacant, its status is changed to reserved and the console output is updated.
+ */
+
+#define _CRT_SECURE_NO_DEPRECATE	// To use scanf() without warnings
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes
+void reserveSeat();
+void printSeatPlan();
+
+int main(void)
+{
+
+	getchar();
+	return 0;
+}
+
+void reserveSeat()
+{
+
+}
+
+void printSeatPlan()
+{
+
+}
