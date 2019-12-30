6cb657e769b63f233454e2dbd26696fd56da6b38
Yuriel849 <yuriel849sp@gmail.com>
2019-12-30 10:25:16 +0100

    printSeatPlan() modified for structure "seatInfo"

--- SO1-Labs/lab6a/lab6a.c
+++ SO1-Labs/lab6a/lab6a.c
@@ -6,11 +6,17 @@ Instructions:
 	Every seat is either vacant or reserved.
 Requirements:
 	Information to be printed to the console:
-		Reservation status (vacant or reserved) for each seat
-		Overall number of vacant seats and number of reserved seats
-	Making reservations:
+		Each seat should show its row number, position A to D within the row, and reservation status (vacant or reserved).
+		Overall number of vacant seats and number of reserved seats.
+		Skip row number 13 (i. e., row 12 is succeeded by row 14).
+	Making reservations						:
 		Initially all seats are vacant.
 		Users are repeatedly prompted to reserve a seat. If vacant, its status is changed to reserved and the console output is updated.
+		Users are allowed to use capital or small letters when reserving a seat (e. g., "7A" or "7a").
+	Exiting the program						:
+		Write the console output to a text file (e. g., flightPlan.txt) before exiting the program.
+			Implement a function that writes the seat plan either to the console or to a text file.
+			Use fprintf() and pass stdout (to write to the console) or the file pointer as argument.
  */
 
 #define _CRT_SECURE_NO_DEPRECATE	// To use scanf() without warnings
@@ -19,18 +25,28 @@ Requirements:
 #include <stdio.h>
 #include <stdlib.h>
 
+// Structure
+typedef struct {
+	int rowNumber; // Row number
+	char position; // Position A to D in a row
+	char status;	   // Reservation status
+} seatInfo;
+
 // Function prototypes
-void reserveSeat(int *seats);
-void printSeatPlan(int *seats);
+void reserveSeat(seatInfo *seats);
+void printSeatPlan(seatInfo *seats);
 void clearBuffer(void);
 
 int main(void)
 {
-	int *seats = (int*)malloc(sizeof(int) * size);
-	for (int seat = 0; seat < size; seat++)
-		*(seats + seat) = seat + 1;
+	seatInfo *seats = (seatInfo*)malloc(sizeof(seatInfo) * size);
+	for (int row = 0; row < size / 4 + 1; row++)		 // row		 == 0 ~ 12
+		for (int position = 0; position < 4; position++) // position == 0 ~ 3
+			if(4 * row + position < 50) // Because the last row has only two seats 
+				*(seats + (4 * row) + position) = (seatInfo) { (row == 12) ? 14 : row + 1, 'A' + position, '*' };
 
-	reserveSeat(seats);
+	printSeatPlan(seats);
+	//reserveSeat(seats);
 
 	free(seats);
 	getchar();
@@ -48,17 +64,17 @@ void reserveSeat(int *seats)
 	{
 		printf("Reserve seat(s) (q to quit):");
 
-		if (scanf("%d", &toReserve) != 1) // For all non-integer input
+		if (scanf("%d", &toReserve) != 1)				// For all non-integer input
 		{
-			if (getchar() == 'q') // If 'q' was entered, scanf() returns 0 and 'q' remains in the input buffer
+			if (getchar() == 'q')
 				break;
 			printf("Please enter a valid seat number.\n");
 		}
-		else if (!(toReserve > 0 && toReserve <= 50)) // For integers outside the range of 1-50
+		else if (!(toReserve > 0 && toReserve <= 50))	// For integers outside the range of 1-50
 			printf("Please enter a valid seat number.\n");
 		else if (seats[toReserve - 1] > 0 && seats[toReserve - 1] <= 50) // If the value of the desired seat is positive, it is vacant.
 		{
-			seats[toReserve - 1] *= -1; // Multiply -1 to the value of the desired seat, indicating it is now reserved.
+			seats[toReserve - 1] *= -1; // Negative integer indicates reserved seat.
 			printSeatPlan(seats);
 		}
 		else
@@ -68,34 +84,34 @@ void reserveSeat(int *seats)
 	}
 }
 
-void printSeatPlan(int *seats)
+void printSeatPlan(seatInfo *seats)
 {
-	int flag = 0, counter = 0;
+	int counter = 0;
 
 	system("cls");
-	printf("Seating plan Bombardier CRJ-200\n   /                \\   \n  /                  \\  \n +                    + \n");
-	for (int seat = 0; seat < 50; seat += 4)
+	printf(" Seating plan Bombardier CRJ-200\n    /                   \\   \n   /                     \\  \n  +                       + \n");
+	for (int row = 0; row < size / 4 + 1; row++)
 	{
-		printf(" |  ");
-		for (int column = 0; column < 4; column++)
+		printf("  | ");
+	
+		for (int position = 0; position < 4; position++)
 		{
-			if (seat + column == 50)
+			if (4 * row + position < 50)
 			{
-				printf("        ");
+				printf("%3d%c%c", seats[(4 * row) + position].rowNumber, seats[(4 * row) + position].position, seats[(4 * row) + position].status);
+				if ((seats[(4 * row) + position].status == ' '))
+					counter++;
+			}
+			else
+			{
+				printf("          ");
 				break;
 			}
-			if (*(seats + seat + column) < 0)
-			{
-				flag = 1;
-				counter++;
-			}
-			printf("%3d%c", (flag == 1) ? *(seats + seat + column) * -1 : *(seats + seat + column), (flag == 1) ? ' ' : '*');
-			flag = 0;
 		}
 
 		printf("  | \n");
 	}
-	printf(" | %2d reserved, %2d vacant*  \n", counter, size - counter);
+	printf("  | %2d reserved, %2d vacant*  \n", counter, size - counter);
 }
 
 void clearBuffer(void)
