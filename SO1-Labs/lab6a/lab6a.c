87abd17db61241e868d3f9c0d13b170d1e3edf88
Yuriel849 <yuriel849sp@gmail.com>
2020-01-01 19:23:02 +0100

    code simplified and comments edited

--- SO1-Labs/lab6a/lab6a.c
+++ SO1-Labs/lab6a/lab6a.c
@@ -9,7 +9,7 @@ Requirements:
 		Each seat should show its row number, position A to D within the row, and reservation status (vacant or reserved).
 		Overall number of vacant seats and number of reserved seats.
 		Skip row number 13 (i. e., row 12 is succeeded by row 14).
-	Making reservations						:
+	Making reservations with user input		:
 		Initially all seats are vacant.
 		Users are repeatedly prompted to reserve a seat. If vacant, its status is changed to reserved and the console output is updated.
 		Users are allowed to use capital or small letters when reserving a seat (e. g., "7A" or "7a").
@@ -19,8 +19,8 @@ Requirements:
 			Use fprintf() and pass stdout (to write to the console) or the file pointer as argument.
  */
 
-#define _CRT_SECURE_NO_DEPRECATE	// To use scanf() without warnings
-#define size 50						// The number of seats in the CRJ-200 aircraft
+#define _CRT_SECURE_NO_DEPRECATE			// To use scanf() without warnings
+#define size 50								// The number of seats in the CRJ-200 aircraft
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -49,7 +49,6 @@ int main(void)
 			if(4 * row + position < 50) // Because the last row has only two seats 
 				*(seats + (4 * row) + position) = (seatInfo) { (row == 12) ? 14 : row + 1, 'A' + position, '*' };
 
-	//printSeatPlan(seats);
 	reserveSeat(seats);
 
 	free(seats);
@@ -59,42 +58,49 @@ int main(void)
 
 void reserveSeat(seatInfo *seats)
 {
-	int rowReserve = 0;
-	char positionReserve = ' ';
-	int scanReturn = 0;
-	int bufferValue;
+	int rowReserve = 0;			// Row number for the desired seat
+	char positionReserve = ' '; // Position letter for the desired seat
+	int scanReturn = 0;			// Return value of scanf() => How many values were correctly scanned
+	char bufferValue = ' ';		// Value of the input buffer (Exit the program if 'q' or 'Q')
+	int index = 0;				// Value of the chosen row and position as a number between 0 ~ 49
 	
-	printSeatPlan(seats, 's'); // Initially print seating plan for user to see.
+	printSeatPlan(seats, 's');  // Initially print seating plan for user to see.
 	printf("\n");
 	
 	while(1)
 	{
 		printf("Reserve seat(s) (q to quit):");
 		scanReturn = scanf("%d%c", &rowReserve, &positionReserve);
-		if (positionReserve >= 'a' && positionReserve <= 'z') // If the user entered a lowercase letter, change to capital letter
-			positionReserve -= 32;
+		if (positionReserve >= 'a' && positionReserve <= 'z') // If input is a lowercase letter, change to capital letter
+			positionReserve -= 32;							  // 'A' ~ 'Z' == 65 ~ 90, 'a' ~ 'z' == 97 ~ 122
+		index = (rowReserve - 1) * 4 + (positionReserve - 'A');
 
-		if (scanReturn != 2) // If the row number and position letter are not properly scanned
+		if ((scanReturn != 2) ||													  // If the row and position are not properly scanned
+			!((rowReserve > 0 && rowReserve <= 12) || rowReserve == 14) ||			  // If row is not 0 ~ 12 or 14
+			!(positionReserve >= 'A' && positionReserve <= 'D') ||					  // If position is not A ~ D
+			(rowReserve == 14 && positionReserve != 'A' && positionReserve != 'B')) // If row == 14 but position is not A or B
 		{
 			bufferValue = getchar();
-			if (bufferValue == 'q' || bufferValue == 'Q') // User desires to exit the program
+			if (bufferValue == 'q' || bufferValue == 'Q')	  // User desires to exit the program
 			{
-				printf("Thank you for utilizing our services. Auf Wiedersehen!");
-				printSeatPlan(seats, 'q');
+				printf("\nThank you for utilizing our services. Auf Wiedersehen!\n");
+				printSeatPlan(seats, 'q');					  // Write to file "flightPlan.txt" before exiting program
 				break;
 			}
-			printf("Please enter a valid seat number.\n");
+			else
+			{
+				ungetc(bufferValue, stdin);					  // If not 'q' or 'Q', return to input buffer (for cleanBuffer())
+				printf("Please enter a valid seat number.\n");
+			}
 		}
-		else if (!((rowReserve > 0 && rowReserve <= 12) || rowReserve == 14) || !(positionReserve >= 'A' && positionReserve <= 'D') || (rowReserve == 14 && !(positionReserve >= 'A' && positionReserve <= 'B')))
-			printf("Please enter a valid seat number.\n");
-		else if (rowReserve == 14 && (seats[(rowReserve - 2) * 4 + (positionReserve - 'A')].status == '*'))
+		else if (rowReserve == 14 && (seats[index - 4].status == '*'))
 		{
-			seats[(rowReserve - 2) * 4 + (positionReserve - 'A')].status = ' ';
+			seats[index - 4].status = ' ';
 			printSeatPlan(seats, 's');
 		}
-		else if (seats[(rowReserve - 1) * 4 + (positionReserve - 'A')].status == '*')
+		else if (seats[index].status == '*')
 		{
-			seats[(rowReserve - 1) * 4 + (positionReserve - 'A')].status = ' ';
+			seats[index].status = ' ';
 			printSeatPlan(seats, 's');
 		}
 		else
@@ -106,12 +112,12 @@ void reserveSeat(seatInfo *seats)
 
 void printSeatPlan(seatInfo *seats, char printMode)
 {
-	int counter = 0;
+	int counter = 0, index = 0; // counter is for counting number of reserved seats, index is for using the seatInfo array
 	FILE *file = fopen("flightPlan.txt", "w");
 
-	if (printMode == 's') // stdout
+	if (printMode == 's')		// stdout, print to console
 	{
-		system("cls");
+		system("cls");			// Clear the console before printing
 		fprintf(stdout, " Seating plan Bombardier CRJ-200\n    /                   \\   \n   /                     \\  \n  +                       + \n");
 		for (int row = 0; row < size / 4 + 1; row++)
 		{
@@ -119,13 +125,14 @@ void printSeatPlan(seatInfo *seats, char printMode)
 	
 			for (int position = 0; position < 4; position++)
 			{
-				if (4 * row + position < 50)
+				index = 4 * row + position;
+				if (index < 50)
 				{
-					fprintf(stdout, "%3d%c%c", seats[(4 * row) + position].rowNumber, seats[(4 * row) + position].position, seats[(4 * row) + position].status);
-					if ((seats[(4 * row) + position].status == ' '))
+					fprintf(stdout, "%3d%c%c", seats[index].rowNumber, seats[index].position, seats[index].status);
+					if ((seats[index].status == ' '))
 						counter++;
 				}
-				else
+				else // For the 13th row, where there are only two seats
 				{
 					fprintf(stdout, "          ");
 					break;
@@ -136,7 +143,7 @@ void printSeatPlan(seatInfo *seats, char printMode)
 		}
 		fprintf(stdout, "  | %2d reserved, %2d vacant*  \n", counter, size - counter);
 	}
-	else if (file != NULL) // print to file
+	else if (file != NULL)		// write to file, prior to exiting program
 	{
 		fprintf(file, " Seating plan Bombardier CRJ-200\n    /                   \\   \n   /                     \\  \n  +                       + \n");
 		for (int row = 0; row < size / 4 + 1; row++)
@@ -145,13 +152,14 @@ void printSeatPlan(seatInfo *seats, char printMode)
 	
 			for (int position = 0; position < 4; position++)
 			{
-				if (4 * row + position < 50)
+				index = 4 * row + position;
+				if (index < 50)
 				{
-					fprintf(file, "%3d%c%c", seats[(4 * row) + position].rowNumber, seats[(4 * row) + position].position, seats[(4 * row) + position].status);
-					if ((seats[(4 * row) + position].status == ' '))
+					fprintf(file, "%3d%c%c", seats[index].rowNumber, seats[index].position, seats[index].status);
+					if ((seats[index].status == ' '))
 						counter++;
 				}
-				else
+				else // For the 13th row, where there are only two seats
 				{
 					fprintf(file, "          ");
 					break;
