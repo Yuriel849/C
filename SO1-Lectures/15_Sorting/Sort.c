983e637e75b9ab9ea700d3f1171540ec59e67dcd
Yuriel849 <yuriel849sp@gmail.com>
2020-01-07 10:52:42 +0100

    problem, timeInMs == 0

--- SO1-Lectures/15_Sorting/Sort.c
+++ SO1-Lectures/15_Sorting/Sort.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SIZE 10
 
 void bubbleSort(int *array, int size);
 void swap(int *first, int *second);
+void singlePassSort(int *array, int size);
 
 int main(void)
 {
 	int toSort[SIZE] = { 666, 123, -141, 3, -777, 1, 105, 106, 0, -787 };
+	clock_t start;
+	unsigned timeInMs;
 
 	for (int index = 0; index < SIZE; index++)
 		printf("%5d", toSort[index]);
 	printf("\n");
 
+	start = clock();
 	bubbleSort(toSort, SIZE);
+	timeInMs = (unsigned)(((clock() - start) * 1000) / CLOCKS_PER_SEC);
 
+	printf("Time taken (bubble sort) : %u ms\n", timeInMs);
 	for (int index = 0; index < SIZE; index++)
 		printf("%5d", toSort[index]);
-
+	
 	getchar();
 	return 0;
 }
 
 // Sort array of size with quicksort
 
+// Sort array of size with single-pass sort
+void singlePassSort(int *array, int size)
+{
+
+}
 
 // Sort array of size with bubble sort
 void bubbleSort(int *array, int size)
