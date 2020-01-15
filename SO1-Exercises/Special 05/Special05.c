d50dd9585ef981aa67bf2297e54f0327142d0b94
Yuriel849 <yuriel849sp@gmail.com>
2020-01-15 15:58:58 +0100

    special exercise 05 - complex number absolute, add, multiple

--- SO1-Exercises/Special 05/Special05.c
+++ SO1-Exercises/Special 05/Special05.c
@@ -1,7 +1,11 @@
 /*
 Author	    : Myungjun Kim
-Contents    : Special exercise 05 - 
+Contents    : Special exercise 05 - Implement functionality for mathematical complex numbers z = x + jy with real part x and imaginary part y. 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 typedef struct {
 	double real;
@@ -9,9 +13,9 @@ typedef struct {
 } complex;
 
 /* Function prototypes */
-double absolute(complex z);
-complex add(complex a, complex b);
-complex multiply(complex a, complex b);
+double absolute(complex z);				// Returns the absolute value |z| = sqrt(x^2 + y^2) of a complex number z.
+complex add(complex a, complex b);		// Returns the sum a + b of two complex numbers a and b.
+complex multiply(complex a, complex b); // Returns the product a * b of two complex numbers a and b.
 
 int main(void)
 {
@@ -38,4 +42,29 @@ int main(void)
 
 	getchar();
 	return 0;
+}
+
+double absolute(complex z)				// Returns the absolute value |z| = sqrt(x^2 + y^2) of a complex number z.
+{
+	return sqrt((z.real * z.real) + (z.imag * z.imag));
+}
+
+complex add(complex a, complex b)		// Returns the sum a + b of two complex numbers a and b.
+{
+	complex result;
+	
+	result.real = a.real + b.real;
+	result.imag = a.imag + b.imag;
+	
+	return result;
+}
+
+complex multiply(complex a, complex b) // Returns the product a * b of two complex numbers a and b.
+{
+	complex result;
+
+	result.real = (a.real * b.real) - (a.imag * b.imag);
+	result.imag = (a.real * b.imag) + (a.imag * b.real);
+
+	return result;
 }
