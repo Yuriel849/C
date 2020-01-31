f07b720049ca9e00f9a13359672f2c6f9085956e
Yuriel849 <yuriel849sp@gmail.com>
2020-01-31 22:33:48 +0100

    comments edited

--- SO1-Lectures/01_Start/start.c
+++ SO1-Lectures/01_Start/start.c
@@ -6,20 +6,18 @@
 		(void) means there is no parameter at all and thus no argument is accepted
  */
 
+ /* To add comments...
+	 Add "//"comment --> ctrl + k + c
+	 Remove "//" comment --> ctrl + k + u
+	 Add & remove comments --> shift + ctrl + /
+  */
+
+  /* NOP */ // => comment indicating there is supposed to be nothing here, "No OPeration"
+
 int main(void) // There can only be ONE(1) main() in a project
 {
 	// all commands within a function are called statements and must end with a semicolon (";")
 	printf("So..... the first \'program\' for this course in C :)");
-	getchar(); /* function defined in stdio.h, forces the program to stop and wait for the user to press "Enter" before continuing;
-					without getchar(), the program would continue to the end of main() and terminate before user can see printf()
-				*/
+	getchar(); // Defined in stdio.h, forces the program to stop and wait for the user to enter something
 	return 0;
 }
-
-/* To add comments...
-	Add "//"comment --> ctrl + k + c
-	Remove "//" comment --> ctrl + k + u
-	Add & remove comments --> shift + ctrl + /
- */
-
- /* NOP */ // => comment indicating there is supposed to be nothing here, "No OPeration"
