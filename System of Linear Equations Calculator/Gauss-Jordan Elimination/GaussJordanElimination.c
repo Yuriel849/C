7e0c3aa08de05ae18371876f10884ff16a872ec0
Yuriel849 <yuriel849sp@gmail.com>
2020-02-04 23:31:13 +0100

    basic set up with comments for functions

---
+++ System of Linear Equations Calculator/Gauss-Jordan Elimination/GaussJordanElimination.c
@@ -0,0 +1,30 @@
+/*
+Author	 : Myungjun Kim
+Contents : Solve a system of linear equations using Gauss-Jordan Elimination, and print each step.
+*/
+
+/* Header files */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+
+/* Main function */
+int main(void)
+{
+
+}
+
+/* Create system of linear equations */
+/* Designate size of system (user input) */
+/* Enter coefficients and solution values (user input) */
+
+/* Find rref form of extended coefficient matrix */
+/* Row multiplication with scalar (pointers) */
+/* Row addition (pointers) */
+/* Row exchange (pointers) */
+
+/* Find solution from rref form of extended coefficient matrix */
+
+/* Print extended coefficient matrix */
+/* Print solution to system */
