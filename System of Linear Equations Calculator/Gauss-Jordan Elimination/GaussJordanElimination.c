9f5c8b8c5af02c36fe9bffe4143693831e4c333d
Yuriel849 <yuriel849sp@gmail.com>
2020-02-05 21:29:39 +0100

    functions to create system and get size from user

--- System of Linear Equations Calculator/Gauss-Jordan Elimination/GaussJordanElimination.c
+++ System of Linear Equations Calculator/Gauss-Jordan Elimination/GaussJordanElimination.c
@@ -12,12 +12,51 @@ Contents : Solve a system of linear equations using Gauss-Jordan Elimination, an
 /* Main function */
 int main(void)
 {
+	double* system;
 
+	createSystem(system);
+
+	free(system);
 }
 
 /* Create system of linear equations */
+void createSystem(double* system)
+{
+	int equations, pointerBytes, dataBytes;
+	equations = getSizeOfSystem();
+
+	pointerBytes = equations * sizeof(double *);
+	dataBytes = (equations + 1) * sizeof(double);
+	if ((system = (double **)malloc(pointerBytes + dataBytes)) == NULL)
+		exit(EXIT_FAILURE);
+
+	getValuesOfSystem(system);
+}
+
 /* Designate size of system (user input) */
+int getSizeOfSystem(void)
+{
+	int equations = 0, scanResult = 0;
+
+	while (1)
+	{
+		printf("Type the number of equations: ");
+		scanResult = scanf("%d", &equations);
+
+		if (!(equations <= 0 || scanResult != 1))
+			break;
+
+		printf("That entry is invalid. Enter the number of equations: ");
+	}
+
+	return equations;
+}
+
 /* Enter coefficients and solution values (user input) */
+void getValuesOfSystem(double* system)
+{
+
+}
 
 /* Find rref form of extended coefficient matrix */
 /* Row multiplication with scalar (pointers) */
