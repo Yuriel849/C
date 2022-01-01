b37e225b1a73b4d77c1a806110debf0af52ca073
Yuriel849 <yuriel849sp@gmail.com>
2022-01-01 02:26:20 +0100

    Add testing for cAtomicData

--- XRAY-Labs/AtomicData_Material/AtomicData_Material/main.cpp
+++ XRAY-Labs/AtomicData_Material/AtomicData_Material/main.cpp
@@ -19,13 +19,13 @@ void testMaterial();
 int main() {
 	// try-catch-throw mechanism
 	try {
-		cout << "Test atomicData and material" << endl;
+		cout << "Test atomicData and material:" << endl;
 
 		testAtomicData();
 
 		testMaterial();
 
-		cout << "successful run" << endl;
+		cout << "Successful run!" << endl;
 	}
 	// Error that throws an object of class exception.
 	// Use exception::what() to get specific error message.
@@ -43,7 +43,34 @@ int main() {
 
 void testAtomicData() {
 	cAtomicData atomic;
+	
+	cout << "  Testing cAtomicData" << endl;
+
+	//================================================================================================================================
+	// Prepare the atomic data.
+	cout << "    Preparing the atomic data." << endl;
 	atomic.prepare();
+
+	//================================================================================================================================
+	// Check method getStdAtomicWeight()
+	cout << "    Comparing actual atomic weights with the atomic weights from the atomic data." << endl;
+	cout << "      Comparing the atomic weights for hydrogen..." << endl;
+	if (!(abs(1.008 - atomic.getStdAtomicWeight(1)) < 0.005))
+		throw runtime_error("Atomic weight for hydrogen is incorrect.");
+	cout << "      Comparing the atomic weights for oxygen..." << endl;
+	if (!(abs(15.999 - atomic.getStdAtomicWeight(8)) < 0.005))
+		throw runtime_error("Atomic weight for oxygen is incorrect.");
+	cout << "      Comparing the atomic weights for silicon..." << endl;
+	if (!(abs(28.085 - atomic.getStdAtomicWeight(14)) < 0.005))
+		throw runtime_error("Atomic weight for silicon is incorrect.");
+	cout << "      => getStdAtomicWeight() tested successfully." << endl;
+
+	//================================================================================================================================
+	// Check method getTotalCrossSection
+	cout << "    Getting the total cross section for Aluminum (atomic number=13) at 27.9keV." << endl;
+	if (!(abs(61.223 - atomic.getTotalCrossSection(13, 27.9)) < 0.005))
+		throw runtime_error("Interpolated total cross section is incorrect.");
+	cout << "      => getTotalCrossSection() tested successfully." << endl;
 }
 
 void testMaterial() {
